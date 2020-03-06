17acf7482ecf742a58b579278a1b256e8406568b
Blair <blairacarroll@gmail.com>
2020-03-06 13:41:09 -0500

    style and color changes to bar graph

--- lofty_client/src/components/Graphpage.jsx
+++ lofty_client/src/components/Graphpage.jsx
@@ -11,11 +11,11 @@ export default class Graphpage extends React.Component {
         let propData = this.props.propData
         console.log(this.props, 'the passed this.props')
         const data = [
-            { text: `Give us a try... ${propData.pitch1}`, value: propData.pitch1 },
-            { text: `Nothing to lose... ${propData.pitch2}`, value: propData.pitch2 },
-            { text: `Ready to move forward... ${propData.pitch3}`, value: propData.pitch3 },
-            { text: `When do we begin... ${propData.pitch4}`, value: propData.pitch4 },
-            { text: `Come onnnnnn ${propData.pitch5}`, value: propData.pitch5 }
+            { text: propData.pitch1, value: propData.pitch1 },
+            { text: propData.pitch2, value: propData.pitch2 },
+            { text: propData.pitch3, value: propData.pitch3 },
+            { text: propData.pitch4, value: propData.pitch4 },
+            { text: propData.pitch5, value: propData.pitch5 }
         ];
     
         const margin = { top: 20, right: 20, bottom: 30, left: 40 };
@@ -24,7 +24,8 @@ export default class Graphpage extends React.Component {
                 <BarChart 
                     ylabel='Closing Pitch'
                     height={500}
-                    width={1175}
+                    width={500}
+                    color='white'
                     margin={margin}
                     data={data}
                     onBarClick={this.handleBarClick}
